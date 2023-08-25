6580f5f21d8cbdb3e3aba9357653adf64332fe17
Iamneo <techadmin@iamneo.ai>
2023-08-25 07:51:08 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -2,9 +2,7 @@ package ai.iamneo.testing.Testing_Selenium_TestNg;
 
 import org.testng.annotations.Test;
 import java.net.URL;
-import java.util.List;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -17,38 +15,49 @@ public class AppTest {
 	ChromeOptions chromeOptions = new ChromeOptions();
 	WebDriver driver = null;
 
-
 	@BeforeTest
-	public void beforeTest() throws Exception {
-		
+	public void beforeTest() throws Exception
+	 {
 		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
-        driver.get("https://flipkart.com/");
-
 	}
 
 	@Test
-	public void testcase_1() throws InterruptedException //Go to Ebay
+//Checking the title of iamNeo (Home - iamneo)
+	public void iamNeo() throws InterruptedException 
 	{
-		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-	    int count= allLinks.size();
 
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
 	}
-
 	@Test
-	public void testcase_2() throws InterruptedException 
-      {
-		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-		for (WebElement link : allLinks) 
-		{
-			System.out.println("Link Text: " + link.getText() + " Link URL: " + link.getAttribute("href"));
-		}
-		
+//Moving to FACEBOOK
+	public void nextPage() throws InterruptedException 
+	{
+		 	
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
+
 	}
-	
-	     
-		
+	@Test
+//Back to iamNeo
+	public void backPage() throws InterruptedException 
+	{
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
+
+	}
+	@Test
+//Current URL
+public void currentURL() throws InterruptedException 
+{
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
+
+}
+
 	@AfterTest
-	public void afterTest() {
+	public void afterTest() 
+	{
 		driver.quit();
 	}
 
